a4c1d0f3c54eb8ed92131f7ab92891b50a53f71a
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-27 12:52:20 +0100

    Created scanStateUpdated function in MainActivityViewModel

--- app/src/main/java/com/example/sugaiot/MainActivityViewModel.kt
+++ app/src/main/java/com/example/sugaiot/MainActivityViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.sugaiot
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
+
+    private val _isScanning = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+    val isScanning: LiveData<Boolean> = _isScanning
+
+    fun scanStateUpdated() {
+        _isScanning.value = !_isScanning.value!!
+    }
 
 }
