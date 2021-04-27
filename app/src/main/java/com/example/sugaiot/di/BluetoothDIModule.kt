f7736172eb278e93ae8b23d47d746471903deb8a
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-27 19:31:53 +0100

    Completed bluetoothDIModule

--- app/src/main/java/com/example/sugaiot/di/BluetoothDIModule.kt
+++ app/src/main/java/com/example/sugaiot/di/BluetoothDIModule.kt
@@ -5,32 +5,41 @@ import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
 import android.content.Context
 import androidx.core.content.ContextCompat.getSystemService
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class BluetoothDIModule {
 
-    fun getBluetoothManager(@ApplicationContext context: Context): BluetoothManager {
+    @Provides
+    @Singleton
+    fun getBluetoothManager(@ApplicationContext context: Context): BluetoothManager? {
         val bluetoothManager: BluetoothManager by lazy {
             context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         }
         return bluetoothManager
     }
 
-    fun getBluetoothAdapter(bluetoothManager: BluetoothManager): BluetoothAdapter {
-        val bluetoothAdapter: BluetoothAdapter by lazy {
-            bluetoothManager.adapter
+    @Provides
+    @Singleton
+    fun getBluetoothAdapter(bluetoothManager: BluetoothManager?): BluetoothAdapter? {
+        val bluetoothAdapter: BluetoothAdapter? by lazy {
+            bluetoothManager?.adapter
         }
         return bluetoothAdapter
     }
 
-    fun getBluetoothLeScanner(bluetoothAdapter: BluetoothAdapter): BluetoothLeScanner{
-        val bluetoothLeScanner : BluetoothLeScanner by lazy{
-            bluetoothAdapter.bluetoothLeScanner
+    @Provides
+    @Singleton
+    fun getBluetoothLeScanner(bluetoothAdapter: BluetoothAdapter?): BluetoothLeScanner? {
+        val bluetoothLeScanner: BluetoothLeScanner? by lazy {
+            bluetoothAdapter?.bluetoothLeScanner
         }
         return bluetoothLeScanner
     }
