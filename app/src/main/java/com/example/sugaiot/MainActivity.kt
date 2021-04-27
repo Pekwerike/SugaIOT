af4c774442ab774cd67704da2874dd588850be37
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-27 12:55:46 +0100

    Added gradle dependencies for lifecycle components

--- app/src/main/java/com/example/sugaiot/MainActivity.kt
+++ app/src/main/java/com/example/sugaiot/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     private var isScanning: Boolean = false
     private val bluetoothLeScanResult: MutableList<ScanResult> = mutableListOf()
+    private val mainActivityViewModel : MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
