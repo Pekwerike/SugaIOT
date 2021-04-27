32c382c023e77da07726d1689ea9b2efa5cdb056
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-27 12:15:34 +0100

    Enabled viewBinding and dataBinding features

--- app/src/main/java/com/example/sugaiot/MainActivity.kt
+++ app/src/main/java/com/example/sugaiot/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.sugaiot
 
+import android.bluetooth.BluetoothManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val bluetoothManager : BluetoothManager by lazy{
+        getSystemService(BluetoothManager::class.java)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
