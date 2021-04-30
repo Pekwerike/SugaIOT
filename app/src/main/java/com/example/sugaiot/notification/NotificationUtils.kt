a531f705919696183a3246d4ed655dc557b38095
Prosper Ekwerike <pekwerike@gmail.com>
2021-04-30 08:27:09 +0100

    Completed NotificationUtils class

--- app/src/main/java/com/example/sugaiot/notification/NotificationUtils.kt
+++ app/src/main/java/com/example/sugaiot/notification/NotificationUtils.kt
@@ -1,6 +1,15 @@
 package com.example.sugaiot.notification
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.example.sugaiot.MainActivity
+import com.example.sugaiot.R
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,6 +19,46 @@ NotificationUtils class is responsible for organizing core functions needed for 
 notification configurations
 */
 @Singleton
-class NotificationUtils @Inject constructor(@ApplicationContext context: Context) {
+class NotificationUtils @Inject constructor(
+    private val notificationManager: NotificationManager,
+    @ApplicationContext private val context: Context
+) {
 
+    fun createCommunicatingWithGlucoseSensorNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val glucoseSensorNotificationChannel = NotificationChannel(
+                context.getString(R.string.communicating_with_glucose_sensor_notification_id),
+                context.getString(R.string.communicating_with_glucose_sensor_notification_channel_name),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+                .apply {
+                    setDescription("This notification channel is responsible for alerting you that ZipBolt is sharing files on the background")
+                    setShowBadge(false)
+                }
+            notificationManager.createNotificationChannel(glucoseSensorNotificationChannel)
+        }
+    }
+
+
+    fun configureCommunicationWithGlucoseSensorNotification(): Notification {
+        val openMainActivityPendingIntent: PendingIntent =
+            Intent(context, MainActivity::class.java).let {
+                it.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+                PendingIntent.getActivity(
+                    context,
+                    1010,
+                    it,
+                    0
+                )
+            }
+
+        return NotificationCompat.Builder(context, context.getString(R.string.communicating_with_glucose_sensor_notification_id))
+            .apply {
+                setContentTitle(context.getString(R.string.communicating_with_glucose_sensor_notification_title))
+                setContentText(context.getString(R.string.communicating_with_glucose_sensor_notification_text))
+                setContentIntent(openMainActivityPendingIntent)
+                setSmallIcon(R.drawable.ic_baseline_ac_unit_24)
+                priority = NotificationCompat.PRIORITY_DEFAULT
+            }.build()
+    }
 }
